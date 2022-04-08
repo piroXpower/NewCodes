c8430739bed4f3de7f55491010a8374681b5dd43
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-08 08:41:30 +0530

    Update chatbot.py

--- FallenRobot/modules/chatbot.py
+++ FallenRobot/modules/chatbot.py
@@ -3,6 +3,7 @@ from os import getenv
 from pyrogram import Client, filters
 from googletrans import Translator
 from FallenRobot import pbot as app
+from FallenRobot import MONGO_DB_URI
 from FallenRobot.decorators.permissions import adminsOnly
 from FallenRobot.utils.filter_groups import *
 # Voics Chatbot Module Credits Pranav Ajay 
