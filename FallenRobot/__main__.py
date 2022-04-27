06b894fd98cb5ffd07333bad7e111bf804e8252b
BlazeOp <94062864+piroXpower@users.noreply.github.com>
2022-04-27 19:34:35 +0530

    Hehe

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -174,7 +174,7 @@ def send_help(chat_id, text, keyboard=None):
     )
 
 
-@run_async
+ 
 def test(update: Update, context: CallbackContext):
     # pprint(eval(str(update)))
     # update.effective_message.reply_text("Hola tester! _I_ *have* `markdown`", parse_mode=ParseMode.MARKDOWN)
@@ -182,7 +182,7 @@ def test(update: Update, context: CallbackContext):
     print(update.effective_message)
 
 
-@run_async
+ 
 def start(update: Update, context: CallbackContext):
     args = context.args
     uptime = get_readable_time((time.time() - StartTime))
@@ -290,7 +290,7 @@ def error_callback(update: Update, context: CallbackContext):
         # handle all other telegram related errors
 
 
-@run_async
+ 
 def help_button(update, context):
     query = update.callback_query
     mod_match = re.match(r"help_module\((.+?)\)", query.data)
@@ -359,7 +359,7 @@ def help_button(update, context):
         pass
 
 
-@run_async
+ 
 def Fallen_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "fallen_":
@@ -384,7 +384,7 @@ def Fallen_about_callback(update: Update, context: CallbackContext):
         )
 
 
-@run_async
+ 
 def Source_about_callback(update: Update, context: CallbackContext):
     query = update.callback_query
     if query.data == "source_":
@@ -409,7 +409,7 @@ def Source_about_callback(update: Update, context: CallbackContext):
                 disable_web_page_preview=False,
         )
 
-@run_async
+ 
 def get_help(update: Update, context: CallbackContext):
     chat = update.effective_chat  # type: Optional[Chat]
     args = update.effective_message.text.split(None, 1)
@@ -510,7 +510,7 @@ def send_settings(chat_id, user_id, user=False):
             )
 
 
-@run_async
+ 
 def settings_button(update: Update, context: CallbackContext):
     query = update.callback_query
     user = update.effective_user
@@ -594,7 +594,7 @@ def settings_button(update: Update, context: CallbackContext):
             LOGGER.exception("Exception in settings buttons. %s", str(query.data))
 
 
-@run_async
+ 
 def get_settings(update: Update, context: CallbackContext):
     chat = update.effective_chat  # type: Optional[Chat]
     user = update.effective_user  # type: Optional[User]
@@ -626,7 +626,7 @@ def get_settings(update: Update, context: CallbackContext):
         send_settings(chat.id, user.id, True)
 
 
-@run_async
+ 
 def donate(update: Update, context: CallbackContext):
     user = update.effective_message.from_user
     chat = update.effective_chat  # type: Optional[Chat]
